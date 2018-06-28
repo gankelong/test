9ce53c239817c304c70b8a40b03ab6606cc4b6f8
gankelong <40172970+gankelong@users.noreply.github.com>
2018-06-28 16:58:42 +0800

    1
    111111

--- src/test/java/com/gs/DemoApplicationTests.java
+++ src/test/java/com/gs/DemoApplicationTests.java
@@ -10,7 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
 	@Test
-	public void contextLoads() 
+	public void contextLoads() {
+		//kelon
 	}
 
 	public void aa(){
